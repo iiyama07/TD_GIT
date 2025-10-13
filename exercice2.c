d9d1401d3b2f20a9062ee94ec4cc8f2c8a6c3bfd
Bordelais <louisbordelais@outlook.fr>
2025-10-13 14:30:37 +0200

    exercice2

---
+++ exercice2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+    int main()
+    {
+        int montantpret = 0;
+        int taux = 0;
+        int duree = 0;
+        printf("Entrez le montant du pret : ");
+        scanf("%d", &montantpret);
+        printf("Entrez le taux d'interet (en pourcentage) : ");
+        scanf("%d", &taux);
+        printf("Entrez la duree du pret (en annees) : ");
+        scanf("%d", &duree);
+        int mensualite = (montantpret * (taux/12))/ (1-(1 + taux/12)^(-duree*12));
+        printf("La mensualite est de %d euros.\n", mensualite);
+        return 0;
+    }
