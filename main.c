3dadde1886d907e9bee946f69067024ba319b0e2
Bordelais <louisbordelais@outlook.fr>
2025-10-13 14:10:20 +0200

    exercice 1

--- main.c
+++ main.c
@@ -2,7 +2,12 @@
 
     int main()
     {
-        printf("Hello");
-
+        int nbseconde = 0;
+        printf("Entrez un nombre de secondes : ");
+        scanf("%d", &nbseconde);    
+        int heures = nbseconde / 3600;
+        int minutes = (nbseconde % 3600) / 60;
+        int secondes = nbseconde % 60;
+        printf("%d secondes correspondent a %d heures, %d minutes et %d secondes.\n", nbseconde, heures, minutes, secondes);
         return 0;
     }
