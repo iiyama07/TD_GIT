e97294f2a6d5267231c24a9b3ac455ccbe8a3c9e
Bordelais <louisbordelais@outlook.fr>
2025-11-03 15:03:12 +0100

    correction exec

--- exec7.c
+++ exec7.c
@@ -3,8 +3,13 @@
 int main() {
     int distance_km = 100;
     int vitesse_kmh = 50;
-    int temps = distance_km / vitesse_kmh; 
+    float temps = distance_km / vitesse_kmh; 
     int minutes = temps * 60;
     printf("minutes = %d\n", minutes); 
     return 0;
 }
+/*
+Le problème dans ce code est que la variable 'temps' est déclarée comme un entier (int)
+ Dans ce cas, la division de 100 par 50 donne 2, ce qui est correct, mais si les valeurs changeaient pour donner un résultat non entier, le résultat serait tronqué.
+ Pour corriger il faut déclarer 'temps' comme un type flottant pour permettre une division décimale précise.
+*/
