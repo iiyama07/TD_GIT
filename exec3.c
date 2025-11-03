56aa7c20fec45df1a9b521207c5d1d3ed472172b
Bordelais <louisbordelais@outlook.fr>
2025-11-03 14:50:53 +0100

    correction exec3

--- exec3.c
+++ exec3.c
@@ -6,3 +6,8 @@ int main() {
     printf("%d\n", y);
     return 0;
 }
+/*
+Le problème dans ce code est que la variable 'x' est utilisée sans avoir été initialisée au préalable.
+ Lorsque cette valeur indéterminée est utilisée dans l'expression 'x + 5', le résultat est également indéterminé.
+ Donc il faut initialiser 'x' avant de l'utiliser, par exemple en lui assignant une valeur comme 'int x = 0;'
+*/
