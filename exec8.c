6d0b56f481ab5d2285b6bd903730c8f02d6271be
Bordelais <louisbordelais@outlook.fr>
2025-11-03 15:05:24 +0100

    correction exec8

--- exec8.c
+++ exec8.c
@@ -2,7 +2,11 @@
 
 int main() {
     int t[3] = {10, 20, 30};
-    int i = -1;
+    int i = 0;
     printf("%d\n", t[i]); 
     return 0;
 }
+/*
+Le problème dans ce code est que l'indice 'i' est initialisé à -1 ce qui est en dehors des limites valides du tableau 't'.
+ Pour corriger ce code, il faut initialiser 'i' avec une valeur valide, par exemple 'int i = 0;' pour accéder au premier élément du tableau.
+*/
