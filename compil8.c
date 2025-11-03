a6f28ce63d15cf1536d0f866e78a57be8f665d72
Bordelais <louisbordelais@outlook.fr>
2025-11-03 14:28:43 +0100

    correction compil8

--- compil8.c
+++ compil8.c
@@ -2,6 +2,12 @@
 
 int main() {
     int x = 10;
-    printf("x = %d\n", y);
+    printf("x = %d\n", x);
     return 0;
 }
+/* 
+error: 'y' undeclared (first use in this function)
+     printf("x = %d\n", y);
+                        ^
+Il faudraist remplacer 'y' par 'x' dans le printf pour corriger l'erreur.
+*/
