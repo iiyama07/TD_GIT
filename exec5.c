1710ac99d8045b8070395b42f6680f4e07dbc610
Bordelais <louisbordelais@outlook.fr>
2025-11-03 14:57:54 +0100

    correction exec5

--- exec5.c
+++ exec5.c
@@ -6,6 +6,12 @@ int main() {
     s[1] = 'e';
     s[2] = 'y';
     s[3] = '!';
+    s[4] = '\0';
     printf("%s\n", s); 
     return 0;
 }
+/*
+Le problème dans ce code est que le tableau de caractères 's' n'est pas correctement terminé par un caractère nul ('\0').
+ Dans ce cas, le tableau 's' a une taille de 5, mais seuls les 4 premiers éléments sont utilisés pour stocker les caractères 'H', 'e', 'y', et '!'.
+ Pour corriger le code il faut ajouter le caractère nul à la fin du tableau par exemple en ajoutant 's[4] = '\0';'
+*/
