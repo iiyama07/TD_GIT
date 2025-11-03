ba1d8f5201a34e3df10712968704d914f03e74ee
Bordelais <louisbordelais@outlook.fr>
2025-11-03 14:38:24 +0100

    correction exec1

--- exec1.c
+++ exec1.c
@@ -2,8 +2,13 @@
 
 int main() {
     int a = 2;
-    int b = 0;
+    int b = 1;
     int c = a / b;  // crash / comportement indéfini
     printf("%d\n", c);
     return 0;
 }
+/*
+ Le code tente de diviser un entier par zéro, ce qui conduit à un comportement indéfini en C. 
+ Cela peut entraîner un crash du programme ou des résultats imprévisibles. 
+ Il faut donc modifier la valeur de b pour qu'elle soit différente de zéro avant la division.
+*/
