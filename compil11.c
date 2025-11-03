9375f897c771ce53bb5a7390cd3c79ce861410bc
Bordelais <louisbordelais@outlook.fr>
2025-11-03 14:35:39 +0100

    correction compil11

--- compil11.c
+++ compil11.c
@@ -3,8 +3,18 @@
 int main() {
     if (1) {
         printf("ok\n");
-    } else
+    } else{
         printf("nope\n");
-        }
+    }
     return 0;
+    
 }
+/*
+ expected identifier or '(' before 'return'
+     return 0;
+     ^~~~~~
+expected identifier or '(' before '}' token
+ }
+ ^
+Il faut ajouter une accolade ouvrante apres le else pour corriger l'erreur.
+*/
