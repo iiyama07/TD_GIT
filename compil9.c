f9582f494ed1ffda22b1e3770c6c1295599435fe
Bordelais <louisbordelais@outlook.fr>
2025-11-03 14:30:16 +0100

    correction compil9

--- compil9.c
+++ compil9.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    char s[3] = "Hello";
+    char s[25] = "Hello";
     printf("%s\n", s);
     return 0;
 }
+/*
+ initializer-string for array of chars is too long
+     char s[3] = "Hello";
+                 ^~~~~~~
+Il faut augmenter la taille du tableau s pour qu'il puisse contenir la chaîne "Hello" et le caractère nul de fin de chaîne.
+*/
