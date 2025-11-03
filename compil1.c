080958e966e436814915bde79ed4e48897a72c78
Bordelais <louisbordelais@outlook.fr>
2025-11-03 14:09:08 +0100

    correction compil1

--- compil1.c
+++ compil1.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello world\n")
+    printf("Hello world\n");
     return 0;
 }
+
+/*compil1.c:5:5: error: expected ';' before 'return'
+     return 0;
+     ^~~~~~
+
+Il manque un point virgule apres le print*/
