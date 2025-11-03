96facbfddaf8736d6a46bbb485610d00cf5e816e
Bordelais <louisbordelais@outlook.fr>
2025-11-03 14:26:22 +0100

    correction compil7

--- compil7.c
+++ compil7.c
@@ -9,6 +9,14 @@ int main() {
     {
         tableau[j] = 1;
     }
-    printf("OK %d\n", tableau[5]);
+    printf("OK %d\n", tableau[4]);
     return 0;
 }
+
+/*
+
+Le code tente d'accéder à un élément en dehors des limites du tableau (tableau[5] alors que les indices valides sont de 0 à 4).
+ En C cela conduit à un comportement indéfini ce qui signifie que le programme peut se comporter de manière imprévisible. 
+ Le compilateur ne signale pas cette erreur car il ne vérifie pas les limites des tableaux à la compilation. 
+ Il faut donc mettre printf("OK %d\n", tableau[4]); a la place de printf("OK %d\n", tableau[5]);
+*/
