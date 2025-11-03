a006190897b93aec55f451aaa38d3c9b0980f8e7
Bordelais <louisbordelais@outlook.fr>
2025-11-03 14:22:28 +0100

    correction compil6

--- compil6.c
+++ compil6.c
@@ -2,8 +2,11 @@
 
 int main() {
     int x = 10;
-    if (x = 5) {
+    if (x == 5) {
         printf("x vaut 5\n");
     }
     return 0;
 }
+/*
+Il faut utiliser '==' pour la comparaison, pas '=' qui est une affectation.
+*/
