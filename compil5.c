55ac9fd51aa0ac624bbf7f7dc700509468a5dd3f
Bordelais <louisbordelais@outlook.fr>
2025-11-03 14:21:00 +0100

    correction compil5

--- compil5.c
+++ compil5.c
@@ -2,6 +2,12 @@
 
 int main() {
     char c = 'A';
-    printf("c = %s\n", c);
+    printf("c = %c\n", c);
     return 0;
 }
+/*
+    expected '%s' directive argument is 'char *', but argument 2 has type 'int'
+        printf("c = %s\n", c);
+                        ^
+    Il faut utiliser %c pour afficher un caractère au lieu de %s qui est utilisé pour les chaînes de caractères.
+*/
