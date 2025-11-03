c95a9ac7efe75fc027cde50424a10e3bf8337908
Bordelais <louisbordelais@outlook.fr>
2025-11-03 14:53:07 +0100

    correction exec4

--- exec4.c
+++ exec4.c
@@ -4,6 +4,12 @@ int main() {
     int i = 0;
     while (i < 10) {
         printf("%d\n", i);
+        i++;
     }
     return 0;
 }
+/*
+Le problème dans ce code est que la variable 'i' n'est jamais incrémentée à l'intérieur de la boucle while.
+ Par conséquent, la condition 'i < 10' reste toujours vraie, ce qui entraîne une boucle infinie.
+ Pour corriger ce code, il faut ajouter une instruction pour incrémenter 'i' à chaque itération de la boucle, par exemple en ajoutant 'i++;' juste avant la fin de la boucle.
+*/
