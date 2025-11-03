7d7ebbe8bf3fa3b0563ff501126994e8e1fa4d69
Bordelais <louisbordelais@outlook.fr>
2025-11-03 14:31:58 +0100

    correction compil10

--- compil10.c
+++ compil10.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int 1abc = 5;
-    printf("%d\n", 1abc);
+    int abc1 = 5;
+    printf("%d\n", abc1);
     return 0;
 }
+/*
+error: invalid suffix "abc" on integer constant
+     int 1abc = 5;
+         ^~~~
+error: expected identifier or '(' before numeric constant
+error: invalid suffix "abc" on integer constant
+     printf("%d\n", 1abc);
+                    ^~~~
+Il faut renommer la variable pour qu'elle commence par une lettre ou un underscore, par exemple en "abc1".
+*/
