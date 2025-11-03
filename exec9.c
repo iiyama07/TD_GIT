9f703d72d4438f5182ff1f14678ddd7bb5aa0bcd
Bordelais <louisbordelais@outlook.fr>
2025-11-03 15:07:08 +0100

    correction exec9

--- exec9.c
+++ exec9.c
@@ -3,6 +3,11 @@
 int main() {
     int a = 10;
     int b = 20;
-    printf("a=%d b=%d\n", b); 
+    printf("a=%d b=%d\n", a, b); 
     return 0;
 }
+/*
+Le problème dans ce code est que dans l'instruction printf, seule la variable 'b' est passée en argument, alors que le format spécifie deux entiers (%d %d).
+ Par conséquent, la valeur de 'a' n'est pas affichée correctement.
+ Pour corriger ce code, il faut ajouter la variable 'a' comme deuxième argument dans l'appel à printf
+ */
