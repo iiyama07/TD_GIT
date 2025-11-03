57b254d7bb61933e8f0664f5f03bcd48a6f09af9
Bordelais <louisbordelais@outlook.fr>
2025-11-03 14:16:15 +0100

    correction compil4

--- compil4.c
+++ compil4.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main {
+int main() {
     printf("Test\n");
     return 0;
 }
+/*
+ expected '=', ',', ';', 'asm' or '__attribute__' before '{' token
+ int main {
+          ^
+Il manque des parenthèses après le main pour indiquer que c'est une fonction.
+*/
