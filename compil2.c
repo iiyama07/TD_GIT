c9baf707ceed47c460ecbf2f0e942074359ef410
Bordelais <louisbordelais@outlook.fr>
2025-11-03 14:11:08 +0100

    correction compil2

--- compil2.c
+++ compil2.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    int x = "42";
+    int x = 42;
     printf("%d\n", x);
     return 0;
 }
+
+ /* initialization makes integer from pointer without a cast [-Wint-conversion]
+     int x = "42";
+             ^~~~
+Il faut enlever les guillemets pour initialiser x avec une valeur entière. */
+ 
