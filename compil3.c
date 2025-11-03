b10927fa354e7177e5c91e134fea303a0134da55
Bordelais <louisbordelais@outlook.fr>
2025-11-03 14:13:37 +0100

    correction compil3

--- compil3.c
+++ compil3.c
@@ -2,7 +2,13 @@
 
 int main() {
     int a;
-    b = 10;
+    int b = 10;
     printf("%d\n", b);
     return 0;
 }
+/*
+error: 'b' undeclared (first use in this function)
+     b = 10;
+     ^
+Il faut déclarer la variable 'b' avant de l'utiliser. Donc mettre un int car b est un entier.
+*/
