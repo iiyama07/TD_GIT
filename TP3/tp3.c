6caf8e788d89cac5ad8b8e80e54975666c3daa82
Bordelais <louisbordelais@outlook.fr>
2025-11-17 13:41:43 +0100

    exercice1-etape4

--- TP3/tp3.c
+++ TP3/tp3.c
@@ -9,22 +9,44 @@ int main() {
         printf("Erreur, veuillez entrer une valeur entre 5 et 10 : ");
         scanf("%d", &n);
     }
+
     int width = n * n;
     int max_height = 3 * (n - 1);
     char img[100][100];
     int i, j;
+
+
     for (i = 0; i < max_height; i++) {
         for (j = 0; j < width; j++) {
             img[i][j] = ' ';
         }
     }
-    printf("\nImage vide (%d x %d) :\n", max_height, width);
+
+    int mid = width / 2;
+
+    for (i = 0; i < n; i++) {
+        int left = mid - i;
+        int right = mid + i;
+
+        for (j = 0; j < width; j++) {
+            if (j == left || j == right) {
+                img[i][j] = 'A';
+            } else if (j > left && j < right) {
+                img[i][j] = 'S';
+            }
+        }
+    }
+
+    img[n-1][mid - n] = '_';
+    img[n-1][mid + n] = '_';
+
+    printf("\nImage (%d x %d) :\n", max_height, width);
     for (i = 0; i < max_height; i++) {
         for (j = 0; j < width; j++) {
             printf("%c", img[i][j]);
         }
         printf("\n");
     }
-    
+
     return 0;
 }
