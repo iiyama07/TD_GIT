0e2de7f48c24b0d812422cfc8000e8c9f51c8796
Bordelais <louisbordelais@outlook.fr>
2025-11-24 13:05:18 +0100

    correction etape 2

--- TP4/tp4.c
+++ TP4/tp4.c
@@ -3,9 +3,9 @@
 // Étape 1 : afficherMenu
 void afficherMenu() {
     printf("===== Gestion des notes =====\n");
-    printf("1. Saisir les notes des élèves\n");
-    printf("2. Afficher les notes des élèves\n");
-    printf("3. Calculer la moyenne d'un élève\n");
+    printf("1. Saisir les notes des eleves\n");
+    printf("2. Afficher les notes des eleves\n");
+    printf("3. Calculer la moyenne d'un eleve\n");
     printf("4. Calculer la moyenne de la classe\n");
     printf("5. Afficher la note maximale\n");
     printf("6. Quitter\n");
