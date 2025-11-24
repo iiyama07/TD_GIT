5fe908d44119a79fe05f5635f8e774a32481aeb4
Bordelais <louisbordelais@outlook.fr>
2025-11-24 13:04:43 +0100

    etape 2 lireChoix

--- TP4/tp4.c
+++ TP4/tp4.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 
-// Étape 1 : fonction afficherMenu
+// Étape 1 : afficherMenu
 void afficherMenu() {
     printf("===== Gestion des notes =====\n");
-    printf("1. Saisir les notes des eleves\n");
-    printf("2. Afficher les notes des eleves\n");
-    printf("3. Calculer la moyenne d'un eleve\n");
+    printf("1. Saisir les notes des élèves\n");
+    printf("2. Afficher les notes des élèves\n");
+    printf("3. Calculer la moyenne d'un élève\n");
     printf("4. Calculer la moyenne de la classe\n");
     printf("5. Afficher la note maximale\n");
     printf("6. Quitter\n");
 }
 
+// Étape 2 : lireChoix
+int lireChoix() {
+    int choix;
+    printf("Votre choix : ");
+    scanf("%d", &choix);
+    return choix;
+}
+
 int main() {
-    // affichage menu
     afficherMenu();
+    int choix = lireChoix();
+    printf("Vous avez choisi : %d\n", choix); 
     return 0;
 }
