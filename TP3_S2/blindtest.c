02c45e7d84247137f81386f34dd3eceeacfc4123
Bordelais <louisbordelais@outlook.fr>
2026-03-21 21:41:33 +0100

    fin de jeu

--- TP3_S2/blindtest.c
+++ TP3_S2/blindtest.c
@@ -29,14 +29,30 @@ int main() {
     Joueur joueur;
     gestion_du_joueur(&joueur);
 
-    //play un extrait
+    // play un extrait et demander le titre
+    char reponse[256];
+    int score = 0;
 
     for (int i = 0; i < count; i++) {
-    printf("\nExtrait %d/%d : %s\n", i + 1, count, song[i].title);
-    play_song_excerpt_at(song[i].file, 0, 10);  
-    } 
+        printf("\nExtrait %d/%d\n", i + 1, count);
+        play_song_excerpt_at(song[i].file, 0, 10);
 
+        printf("Quel est le titre de la chanson ? ");
+        if (fgets(reponse, sizeof(reponse), stdin) == NULL) {
+            printf("Erreur lecture reponse.\n");
+            continue;
+        }
+        trim_newline(reponse);
 
+        if (string_equals_normalized(reponse, song[i].title)) {
+            printf("Bonne reponse !\n");
+            score++;
+        } else {
+            printf("Mauvaise reponse. Le titre correct etait : %s\n", song[i].title);
+        }
+    }
+
+    printf("\nJeu Termine. %s, votre score est %d/%d.\n", joueur.nom, score, count);
 
     return 0;
 }
